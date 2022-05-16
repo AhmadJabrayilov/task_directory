247320fae5078c2386686de5f2929bf60b261ab4
Ahmad Jabrayilov <tu25tyfpp@aztu.local>
2022-05-16 08:16:17 +0400

    featured task

---
+++ Directory_file/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Directory_file.Models
+{
+    class Employee
+    {
+        private int _id;
+        public int Id {get;}
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Directory_file/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Directory_file.Models;
+using Newtonsoft.Json;
+
+namespace Directory_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee {
+                Age = 25,
+                Name = "lorem",
+                Surname = "ipsum"
+                
+            };
+
+            Department department = new Department
+            {   
+                Id =1,
+                Name = "ABC",
+                Employees = new List<Employee>
+                {
+                    employee
+                }
+            };
+            var serialize = JsonConvert.SerializeObject(department);
+
+            using (StreamWriter sr =  new StreamWriter(@"C:\Users\tu25tyfpp\Desktop\6-7_Practic\Directory_file\Directory_file\Data\json1.json") )
+            {
+                sr.WriteLine(serialize);
+            }
+
+            string result;
+
+
+            using (StreamReader dr = new StreamReader(@"C:\Users\tu25tyfpp\Desktop\6-7_Practic\Directory_file\Directory_file\Data\json1.json"))
+            {
+                result =dr.ReadToEnd();
+            }
+
+            Department deserialize = JsonConvert.DeserializeObject<Department>(result);
+            Console.WriteLine(deserialize.Name);
+        }
+
+    }
+}
